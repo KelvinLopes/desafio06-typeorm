d6055adff30114b7787581900fb1903b1f2d9231
Kelvin Lopes <kelvin7android@gmail.com>
2020-10-28 04:15:55 -0300

    Updated database/index.ts

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,10 +8,10 @@ export default async (name = 'default'): Promise<Connection> => {
       name,
       database:
         process.env.NODE_ENV === 'test'
-          ? 'gostack_desafio06'
+          ? 'gostack_desafio06_tests'
           : defaultOptions.database,
     }),
   );
 };
 
-//Testes local: gostack_desafio06_tests
+//Testes local: gostack_desafio06
